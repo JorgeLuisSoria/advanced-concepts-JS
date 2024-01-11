9b40de488afb8f8f027158c33efc8f6f87c5af48
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-11 17:01:57 -0500

    callback updated

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -9,7 +9,14 @@ import { heroes } from "../data/heroes"
 export const callbacksComponent = ( element ) => {
 
     const id = "5d86371f25a058e5b1c8a65e"
-    findHero( id, ( hero ) => {
+    findHero( id, ( error, hero ) => {
+
+        // element.innerHTML = hero?.name || 'No hay heroe';
+
+        if ( error ) {
+            element.innerHTML = error;
+            return;
+        }
 
         element.innerHTML = hero.name;
 
@@ -20,13 +27,18 @@ export const callbacksComponent = ( element ) => {
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object) => void } callback 
+ * @param { (error: String|Null, hero: Object) => void } callback 
  */
 
 const findHero = ( id, callback ) => {
 
     const hero = heroes.find( hero => hero.id === id );
+
+    if ( !hero ) {
+        callback( `Hero with id ${ id } not found.` );
+        return;
+    }
     
-    callback( hero );
+    callback( null, hero );
 
 }
