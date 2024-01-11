b31412398b99dd37744a667deae745cfd9c764d2
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-11 13:46:30 -0500

    callback created

---
+++ src/concepts/02-callbacks.js
@@ -0,0 +1,32 @@
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+import { heroes } from "../data/heroes"
+
+export const callbacksComponent = ( element ) => {
+
+    const id = "5d86371f25a058e5b1c8a65e"
+    findHero( id, ( hero ) => {
+
+        element.innerHTML = hero.name;
+
+    } );
+
+}
+
+/**
+ * 
+ * @param {String} id 
+ * @param { (hero: Object) => void } callback 
+ */
+
+const findHero = ( id, callback ) => {
+
+    const hero = heroes.find( hero => hero.id === id );
+    
+    callback( hero );
+
+}
