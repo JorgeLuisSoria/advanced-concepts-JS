80513fabb3ad123d7bdf1136d1b5857e1d9f3a66
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-11 11:51:06 -0500

    env variables added

--- src/concepts/01-environments.js
+++ src/concepts/01-environments.js
@@ -6,9 +6,15 @@
 
 export const environmentsComponent = ( element ) => {
 
+    // console.log( process.env ); En las aplicaciones de Node se puede acceder a este objeto global (Depende del framework)
+    console.log( import.meta.env );
+
     const html = `
-        variables:
-    `
+        Dev: ${ import.meta.env.DEV } <br/>
+        Prod: ${ import.meta.env.DEV } <br/>
+        KEY: ${ import.meta.env.VITE_API_KEY } <br/>
+        URL: ${ import.meta.env.VITE_BASE_URL } <br/>
+    `;
     element.innerHTML = html;
 
 }
