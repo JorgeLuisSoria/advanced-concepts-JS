e3bac3e3df519bcbaaee5b8923d51d41b45aaa5e
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-12 22:06:32 -0500

    async await added

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import { environmentsComponent } from './src/concepts/01-environments'
 import { callbacksComponent } from './src/concepts/02-callbacks'
 import { promiseComponent } from './src/concepts/03-promises'
 import { promiseRaceComponent } from './src/concepts/04-promise-race'
+import { asyncComponent } from './src/concepts/05-async'
+import { asyncAwaitComponent } from './src/concepts/06-async-await'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -27,4 +29,6 @@ const element = document.querySelector('.card');
 // environmentsComponent( element );
 // callbacksComponent( element );
 // promiseComponent( element );
-promiseRaceComponent( element );
+// promiseRaceComponent( element );
+// asyncComponent( element );
+asyncAwaitComponent( element );
