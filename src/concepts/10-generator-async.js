2669486436ebbda70dff6c39c461c1ef5334fe03
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-13 21:28:23 -0500

    generator functions async

--- src/concepts/10-generator-async.js
+++ src/concepts/10-generator-async.js
@@ -1,11 +1,43 @@
+import { heroes } from "../data/heroes";
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
 
-export const generatorAsyncComponent = ( element ) => {
+export const generatorAsyncComponent = async( element ) => {
 
-    console.log('generatorAsyncComponent');
+    const heroGenerator = getHeroGenerator();
+    let isFinished = false;
 
+    do {
+        
+        const { value, done } = await heroGenerator.next();
+        isFinished = done;
+
+        if ( isFinished ) break;
+
+        element.innerHTML = value;
+
+    } while ( !isFinished );
+
+}
+
+async function* getHeroGenerator() {
+    
+    for( const hero of heroes ) {
+        await sleep();
+        yield hero.name;
+    }
+
+    return 'No hay más';
+
+}
+
+const sleep = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+           resolve(); 
+        }, 1000);
+    })
 }
